ced5a656586adec74703cb7ddebc146a60d9944f
Radmir <gerasimoffdg@gmail.com>
2021-08-20 16:29:22 +0300

    qwerty

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,24 @@
+package org.example;
+
+
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        System.out.println("test2");
+    }
+
+    @Test
+    public void shouldAnswerWithTrue2() {
+        System.out.println("test2");
+    }
+}
---
+++ src/test/java/org/example/IntegrationTest.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.junit.Test;
+
+public class IntegrationTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        System.out.println("Integration test1");
+    }
+
+    @Test
+    public void shouldAnswerWithTrue2() {
+        System.out.println("integration test2");
+    }
+}
